6be85bf64394fc327fc51f0f7ae8a1c341cdc20a
Luis De Anda <ldeandac16@gmail.com>
2020-04-02 13:39:17 -0600

    Create constants.js

---
+++ js/constants.js
@@ -0,0 +1,9 @@
+const BASE_API_URL = 'https://corona.lmao.ninja';
+
+// DOM elements
+const tblCountryCases = document.getElementById('countryCasesTable');
+const divLastUpdated = document.getElementById('lastUpdated');
+const divGlobalConfirmedCases = document.getElementById('globalConfirmedCases');
+const divGlobalActiveCases = document.getElementById('globalActiveCases');
+const divGlobalRecovered = document.getElementById('globalRecovered');
+const divGlobalDeaths = document.getElementById('globalDeaths');
